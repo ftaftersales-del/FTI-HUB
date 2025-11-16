3d8bd1fdc2ddc3f35959a7cdab4cf63814a3f31f
ftaftersales-del <ft.aftersales@gmail.com>
2025-11-16 21:49:22 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+// Sostituisci con i tuoi dati da Firebase
+const firebaseConfig = {
+  apiKey: "TUO_API_KEY",
+  authDomain: "TUA_AUTH_DOMAIN",
+  projectId: "TUO_PROJECT_ID",
+  // ...
+};
+firebase.initializeApp(firebaseConfig);
