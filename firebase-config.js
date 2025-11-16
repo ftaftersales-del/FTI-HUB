9c42d532ada32ca580234647ffc502cc141ecb50
ftaftersales-del <ft.aftersales@gmail.com>
2025-11-16 22:15:30 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 // Sostituisci con i tuoi dati da Firebase
 const firebaseConfig = {
-  apiKey: "TUO_API_KEY",
-  authDomain: "TUA_AUTH_DOMAIN",
-  projectId: "TUO_PROJECT_ID",
+  apiKey: "AIzaSyB9eQULzP4E2NfYc7CS-N0bKMMTMV0a-HM",
+  authDomain: "ft-hub-797eb.firebaseapp.com",
+  projectId: "ft-hub-797eb",
   // ...
 };
 firebase.initializeApp(firebaseConfig);
