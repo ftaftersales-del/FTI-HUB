ebfd4fca22e0c87a6d67d5d7c6ea167c926ee3f8
ftaftersales-del <ft.aftersales@gmail.com>
2025-11-19 21:21:50 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,13 @@
-// Sostituisci con i tuoi dati da Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyB9eQULzP4E2NfYc7CS-N0bKMMTMV0a-HM",
   authDomain: "ft-hub-797eb.firebaseapp.com",
   projectId: "ft-hub-797eb",
-  // ...
+  // qui nella realtà metti anche gli altri campi
+  // (storageBucket, messagingSenderId, appId, ecc.)
 };
+
 firebase.initializeApp(firebaseConfig);
+
+// comodo per il resto del codice:
+const auth = firebase.auth();
+const db = firebase.firestore();
