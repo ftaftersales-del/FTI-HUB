642f3da64b1cdfd6a832a17be77c6d1d751be7e0
ftaftersales-del <ft.aftersales@gmail.com>
2025-11-29 20:22:41 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,18 @@
+// firebase-config.js
+
 const firebaseConfig = {
   apiKey: "AIzaSyB9eQULzP4E2NfYc7CS-N0bKMMTMV0a-HM",
   authDomain: "ft-hub-797eb.firebaseapp.com",
   projectId: "ft-hub-797eb",
-  // qui nella realtà metti anche gli altri campi
-  // (storageBucket, messagingSenderId, appId, ecc.)
+  storageBucket: "ft-hub-797eb.firebasestorage.app",   // <--- QUESTA È LA CHIAVE
+  // se li hai, puoi (ma non è obbligatorio per Storage) aggiungere anche:
+  // messagingSenderId: "XXXXXXX",
+  // appId: "1:XXXXXXX:web:YYYYYYYY"
 };
 
+// inizializzazione condivisa per tutto il portale
 firebase.initializeApp(firebaseConfig);
 
-// comodo per il resto del codice:
+// comodo per il resto del codice globale:
 const auth = firebase.auth();
-const db = firebase.firestore();
+const db   = firebase.firestore();
